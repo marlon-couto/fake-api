24cc5173ea765ff5f11ea55c07bccc7a7857c8b5
Marlon Couto <marloncouto.dev@gmail.com>
2023-02-05 12:26:18 -0300

    Atualiza configurações do servidor

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server');
+const clone = require('clone');
+const data = require('./db.json');
 
+const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
+  _isFake: isProductionEnv,
+});
+
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
@@ -15,9 +22,18 @@ server.use(
   })
 );
 
+
+server.use((req, res, next) => {
+  if (req.path !== '/') {
+    router.db.setState(clone(data));
+  }
+
+  next();
+});
+
 server.use(router);
 
-server.listen(3000, () => {
+server.listen(process.env.PORT || 8000, () => {
   console.log('JSON Server is running');
 });
 
